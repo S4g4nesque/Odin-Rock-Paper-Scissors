e1f97de57a62263b8d22a96d9579a0f3897b56ad
Pomodoro <169356952+S4g4nesque@users.noreply.github.com>
2025-05-01 15:54:30 +0200

    Failed creating function for computer output Tried creating function to output strings using math.random method but not working so far

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 let greeting = "Hello World!";
 
-console.log(greeting);
+console.log(greeting)
+
+function getComputerChoice(max){
+
+     Math.floor(Math.random() * max);
+    if (getComputerChoice === 0){
+        ;
+        return "Rock"
+    }
+    else if (getComputerChoice === 1){
+        return "Paper"
+    }
+    else if (getComputerChoice === 2){
+        return "Scissors"
+    }
+    else {
+        return "Error"
+    }
+
+    }
+
+
+console.log(getComputerChoice(3))
