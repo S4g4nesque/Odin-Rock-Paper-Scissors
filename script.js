4df264f763df94da9c2345db4058d5a7b139981c
Pomodoro <169356952+S4g4nesque@users.noreply.github.com>
2025-05-01 16:01:56 +0200

    Rock paper scissors function working Key: declared new value within fuction, value was defined by math.random method, used the value in if/else conditions

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ console.log(greeting)
 
 function getComputerChoice(max){
 
-    return Math.floor(Math.random() * max);
-    // if (getComputerChoice === 0){
+    let computerAnswer = Math.floor(Math.random() * max);
+    if (computerAnswer === 0){
         
-    //     return "Rock"
-    // }
-    // else if (getComputerChoice === 1){
-    //     return "Paper"
-    // }
-    // else if (getComputerChoice === 2){
-    //     return "Scissors"
-    // }
-    // else {
-    //     return "Error"
-    // }
+        return "Rock"
+    }
+    else if (computerAnswer === 1){
+        return "Paper"
+    }
+    else if (computerAnswer === 2){
+        return "Scissors"
+    }
+    else {
+        return "Error"
+    }
 
     }
 
