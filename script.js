a43bbe7a75c4155233b4fb1e14c0451f5cdb48c1
Pomodoro <169356952+S4g4nesque@users.noreply.github.com>
2025-05-01 15:58:03 +0200

    Function works if just returning math.random Function and method are clearly working. Unable to assign strings to integer values given (eg. 0 = Rock)

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ console.log(greeting)
 
 function getComputerChoice(max){
 
-     Math.floor(Math.random() * max);
-    if (getComputerChoice === 0){
-        ;
-        return "Rock"
-    }
-    else if (getComputerChoice === 1){
-        return "Paper"
-    }
-    else if (getComputerChoice === 2){
-        return "Scissors"
-    }
-    else {
-        return "Error"
-    }
+    return Math.floor(Math.random() * max);
+    // if (getComputerChoice === 0){
+        
+    //     return "Rock"
+    // }
+    // else if (getComputerChoice === 1){
+    //     return "Paper"
+    // }
+    // else if (getComputerChoice === 2){
+    //     return "Scissors"
+    // }
+    // else {
+    //     return "Error"
+    // }
 
     }
 
